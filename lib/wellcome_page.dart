e3e4d8d6c43a87b3db591ab437dfd4cdca321360
Abdul Wahab <abdulwahab1539@gmail.com>
2022-09-17 16:09:45 +0500

    Crash Fixed

--- lib/wellcome_page.dart
+++ lib/wellcome_page.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:pos_system/auth_controller.dart';
-import 'package:pos_system/signup_page.dart';
 
 class WellcomePage extends StatefulWidget {
   String email;
+
   WellcomePage({Key? key, required this.email}) : super(key: key);
 
   @override
@@ -14,18 +12,18 @@ class WellcomePage extends StatefulWidget {
 }
 
 class _WellcomePageState extends State<WellcomePage> {
-
   // List of document ids
   List<String> docIDs = [];
 
   // get Document IDs
   Future getDocID() async {
+    print('getDocID');
     await FirebaseFirestore.instance.collection('customers').get().then(
-            (snapshot) => snapshot.docs.forEach((document) {
-              print(document.reference);
-              docIDs.add(document.reference.id);
-            }),
-    );
+          (snapshot) => snapshot.docs.forEach((document) {
+            print('${snapshot.size}');
+            docIDs.add(document.reference.id);
+          }),
+        );
   }
 
   @override
@@ -48,40 +46,38 @@ class _WellcomePageState extends State<WellcomePage> {
             Container(
               width: width,
               height: height * 0.35,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage(
                     'img/signin_btn.jpg',
-
                   ),
                   fit: BoxFit.cover,
                 ),
               ),
               child: Column(
                 children: [
-                  SizedBox(height: height*0.14,),
-                  CircleAvatar(
+                  SizedBox(
+                    height: height * 0.14,
+                  ),
+                  const CircleAvatar(
                     radius: 60,
                     backgroundColor: Colors.white70,
-                    backgroundImage: AssetImage(
-                        'img/profile.jpg'
-                    ),
+                    backgroundImage: AssetImage('img/profile.jpg'),
                   ),
                 ],
               ),
-
-
-
             ),
-            SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             Container(
               width: width,
               margin: const EdgeInsets.only(left: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Wellcome',
+                  const Text(
+                    'Welcome',
                     style: TextStyle(
                       fontSize: 36,
                       fontWeight: FontWeight.bold,
@@ -98,49 +94,49 @@ class _WellcomePageState extends State<WellcomePage> {
                 ],
               ),
             ),
-            SizedBox(height: 10),
-            Expanded(
-                child: Container(
-                  height: height,
-                  child: FutureBuilder(
-                      future: getDocID(),
-                      builder: (context, snapshot){
-                        if(snapshot.connectionState == ConnectionState.waiting) {
-                          return ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: docIDs.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(docIDs[index]),
-                              );
-                            },
+            const SizedBox(height: 10),
+            Container(
+              height: height,
+              child: FutureBuilder(
+                  future: getDocID(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        //TODO: This line is causing issue
+                        // scrollDirection: Axis.horizontal,
+                        itemCount: docIDs.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(docIDs[index]),
                           );
-                        }else{
-                          return CircularProgressIndicator();
-                        }
-                      }),
-                ),
-              ),
-
-            SizedBox(height: 10),
+                        },
+                      );
+                    } else {
+                      return const CircularProgressIndicator(
+                        color: Colors.black,
+                      );
+                    }
+                  }),
+            ),
+            const SizedBox(height: 10),
             GestureDetector(
-              onTap: (){
+              onTap: () {
                 AuthController.instance.logout();
               },
               child: Container(
-                width: width*0.5,
-                height: height*0.08,
+                width: width * 0.5,
+                height: height * 0.08,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
-                  image: DecorationImage(
+                  image: const DecorationImage(
                     image: AssetImage(
                       'img/signin_btn.jpg',
-
                     ),
                     fit: BoxFit.cover,
                   ),
                 ),
-                child: Center(
+                child: const Center(
                   child: Text(
                     'Sign Out',
                     style: TextStyle(
@@ -148,19 +144,12 @@ class _WellcomePageState extends State<WellcomePage> {
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
-
                   ),
-                  
                 ),
-                              
-
-
-
               ),
             ),
 
-           
-           // SizedBox(height: width*0.08,),
+            // SizedBox(height: width*0.08,),
             // RichText(text: TextSpan(
             //   text: 'Sign Up using one the following methode',
             //   style: TextStyle(
@@ -169,8 +158,6 @@ class _WellcomePageState extends State<WellcomePage> {
             //   ),
             //
             // )),
-
-
           ],
         ),
       ),
